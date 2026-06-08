4421928e018b03a42dea5e458390d70d07d04c51
nicolaugamorim-source <nicolau.g.amorim@gmail.com>
2026-06-08 19:59:03 +0100

    Fix: button variant type error

--- components/landing/hero-flowtex.tsx
+++ components/landing/hero-flowtex.tsx
@@ -45,7 +45,7 @@ interface RotatingTextProps
   texts: string[];
   transition?: Transition;
   initial?: boolean | Target | VariantLabels;
-  animate?: boolean | VariantLabels | AnimationControls | TargetAndTransition;
+  animate?: boolean | VariantLabels | TargetAndTransition;
   exit?: Target | VariantLabels;
   animatePresenceMode?: "sync" | "wait";
   animatePresenceInitial?: boolean;
